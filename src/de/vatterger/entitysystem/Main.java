99e56137ced87a822fb9a4e436dd087612651220
VaTTeRGeR <flo.schmickmann@googlemail.com>
2015-08-24 19:20:35 +0200

    Kleine Umstrukturierung

---
+++ src/de/vatterger/entitysystem/Main.java
@@ -0,0 +1,333 @@
+package de.vatterger.entitysystem;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import de.vatterger.entitysystem.tools.Bucket;
+import de.vatterger.entitysystem.tools.GridPartitionMap;
+
+public class Main {
+	private static SERVER_STATUS runStatus = SERVER_STATUS.Idle;
+	private static final int MB = 1024 * 1024;
+	
+	private static boolean debug = false;
+	private static UpdateRunnable runnable;
+	private static Thread statusThread;
+	private static Runtime runtime = Runtime.getRuntime();
+
+	private static final JTextArea console = new JTextArea();
+	private static final JLabel statusLabel = new JLabel("Idle");
+	private static final JLabel debugLabel = new JLabel("[--]");
+	
+	public static void main(String[] args) {
+		/*Creating the simulation-thread*/
+		runnable = new UpdateRunnable(new SlimeSlickServer());
+		
+		/*The target-framerate is 20 steps per second*/
+		runnable.setTargetFPS(20);
+				
+		/*Constructing the 640x480 JFrame*/
+		final JFrame frame = new JFrame("SlimeSlick-Server");
+		frame.setSize(640, 480);
+		
+		/*Constructing the JMenuBar*/
+		JMenuBar menuBar = new JMenuBar();
+		
+		/*Constructing the JMenu for starting/stopping the simulation*/
+		JMenu simulationMenu = new JMenu("Simulation");
+
+		/*Constructing the JMenu for Exiting the application*/
+		JMenu fileMenu = new JMenu("File");
+		
+		/*Constructing the JMenu for Exiting the application*/
+		JMenu debugMenu = new JMenu("Debug");
+		
+		/*Constructing the JMenuItem for closing the application*/
+		JMenuItem exitButton = new JMenuItem("Exit");
+		exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				shutdown();
+				frame.dispose();
+				System.exit(0);
+			}
+		});
+		
+		/*Constructing the JMenuItem for deleting the savefiles*/
+		JMenuItem deleteSaveButton = new JMenuItem("Delete save-files");
+		deleteSaveButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				deleteSaveGame();
+			}
+		});
+
+		/*Constructing the Button for clearing the console*/
+		JMenuItem clearConsoleButton = new JMenuItem("Clear console");
+		clearConsoleButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clearConsole();
+			}
+		});
+		
+		/*Constructing the JMenuItem for starting the server*/
+		JMenuItem startButton = new JMenuItem("Start");
+		startButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				start();
+			}
+		});
+		
+		/*Constructing the JMenuItem for stopping the server*/
+		JMenuItem stopButton = new JMenuItem("Stop");
+		stopButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stop();
+			}
+		});
+		
+		/*Constructing the JMenuItem for getting the current update-rate of the runnable*/
+		JMenuItem debugButton = new JMenuItem("Toggle Debug");
+		debugButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				toggleDebug();
+			}
+		});
+		
+		/*Constructing the JMenuItem for getting the current update-rate of the runnable*/
+		JMenuItem fpsButton = new JMenuItem("Update-rate");
+		fpsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				printFPS();
+			}
+		});
+		
+		/*Constructing the JMenuItem for getting the current memory usage*/
+		JMenuItem memButton = new JMenuItem("Memory usage");
+		memButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				printMem();
+			}
+		});
+		
+		/*Constructing the JMenuItem for doing some test with spatialvector3map*/
+		JMenuItem testButton = new JMenuItem("TEST: SpatialMap");
+		testButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GridPartitionMap<String> map = new GridPartitionMap<String>(16, 256);
+				
+				printConsole("Adding stuff");
+
+				map.insert(new Vector2(0f, 0f), "0-0-0");
+				map.insert(new Vector2(4f, 5f), "4-5-0");
+				
+				map.insert(new Vector2(20f, 0f), "20-0-0");
+				map.insert(new Vector2(16f, 17f), "16-17-0");
+				
+				map.insert(new Vector2(19f, 19f), "19-19-0");
+				map.insert(new Vector2(21f, 21f), "21-21-0");
+
+				map.insert(new Vector2(210f, 210f), "210-210-0");
+				
+				Bucket<String> b1 = map.getBucket(10f, 10f);
+				for (int i = 0; i < b1.size(); i++) {
+					printConsole("Bucket for [10,10] has "+b1.get(i));
+				}
+				
+				Bucket<String> b2 = map.getBucketsMerged(new Rectangle(0, 0, 256, 256));
+				for (int i = 0; i < b2.size(); i++) {
+					printConsole("Bucket for [0,0,256,256] has "+b2.get(i));
+				}
+
+				printConsole("Clearing the map");
+				map.clear();
+
+				printConsole("Adding stuff");
+				map.insert(new Vector2(210f, 210f), "210-210-0 v2");
+
+				Bucket<String> b3 = map.getBucketsMerged(new Rectangle(0, 0, 256, 256));
+				for (int i = 0; i < b3.size(); i++) {
+					printConsole("Bucket for [0,0,256,256] has "+b3.get(i));
+				}
+			}
+		});
+
+		fileMenu.add(exitButton);
+		fileMenu.add(clearConsoleButton);
+		simulationMenu.add(startButton);
+		simulationMenu.add(stopButton);
+		simulationMenu.add(deleteSaveButton);
+		simulationMenu.add(testButton);
+		debugMenu.add(debugButton);
+		debugMenu.add(fpsButton);
+		debugMenu.add(memButton);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(simulationMenu);
+		menuBar.add(debugMenu);
+		menuBar.add(statusLabel);
+		menuBar.add(debugLabel);
+		
+		frame.setJMenuBar(menuBar);
+
+		statusLabel.setForeground(Color.LIGHT_GRAY);
+		statusLabel.setText("idle");
+		
+		debugLabel.setForeground(Color.LIGHT_GRAY);
+		
+		JScrollPane scrollPanel = new JScrollPane(console, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		frame.add(scrollPanel);
+		
+		frame.addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {}
+			@Override
+			public void windowIconified(WindowEvent e) {}
+			@Override
+			public void windowDeiconified(WindowEvent e) {}
+			@Override
+			public void windowDeactivated(WindowEvent e) {}
+			@Override
+			public void windowClosed(WindowEvent e) {}
+			@Override
+			public void windowActivated(WindowEvent e) {}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				shutdown();
+				frame.dispose();
+				System.exit(0);
+			}
+		});
+		
+		printConsole("Welcome to SlimeSlick Server GUI");
+		
+		frame.setVisible(true);
+	}
+	
+	private static void startFPSWatch() {
+		statusThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(runStatus == SERVER_STATUS.Run) {
+					debugLabel.setText("["+String.format("%.4f", runnable.getCurrentFPS())+"|"+(runtime.totalMemory()-runtime.freeMemory())/MB+"M]");
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+					}
+				}
+			}
+		});
+		statusThread.start();
+		debugLabel.setForeground(Color.BLACK);
+	}
+	
+	private static void stopFPSWatch() {
+		statusThread.interrupt();
+		debugLabel.setForeground(Color.LIGHT_GRAY);
+	}
+	
+	private static void start() {
+		if (runStatus == SERVER_STATUS.Idle) {
+			printConsole("Starting Server");
+			runnable.setDebug(debug);
+			runnable.startSimulation();
+
+			runStatus = SERVER_STATUS.Run;
+			statusLabel.setForeground(Color.BLUE);
+			statusLabel.setText("Server running");
+			startFPSWatch();
+		} else {
+			printConsole("Server already started.");
+		}
+	}
+	
+	private static void stop() {
+		if(runStatus == SERVER_STATUS.Run) {
+			printConsole("Stopping server");
+			runnable.stopSimulation();
+
+			runStatus = SERVER_STATUS.Idle;
+			statusLabel.setForeground(Color.LIGHT_GRAY);
+			statusLabel.setText("Server stopped");
+			
+			stopFPSWatch();
+		} else {
+			printConsole("No server running");
+		}
+	}
+	
+	private static void shutdown() {
+		stop();
+		printConsole("Shutting down...");
+	}
+	
+	private static void printFPS() {
+		final String sCL;
+		if(runStatus == SERVER_STATUS.Run)
+			sCL = "Current";
+		else
+			sCL = "Last";
+			printConsole(sCL + " FPS: "+runnable.getCurrentFPS()+"\n");
+	}
+	
+	private static void printMem() {
+		printConsole("Used Memory:" + (runtime.totalMemory() - runtime.freeMemory()) / MB);
+		printConsole("Free Memory:" + runtime.freeMemory() / MB);
+		printConsole("Total Memory:" + runtime.totalMemory() / MB);
+		printConsole("Max Memory:" + runtime.maxMemory() / MB+"\n");
+	}
+	
+	private static void toggleDebug(){
+		debug = !debug;
+		runnable.setDebug(debug);
+		
+		if(runnable.getIsDebug())
+			printConsole("Debug enabled.");
+		else
+			printConsole("Debug disabled.");
+	}
+	
+	private static void deleteSaveGame() {
+		FileHandle savegame = new FileHandle("data/kryo.gzip");
+		if (savegame.exists()) {
+			savegame.delete();
+			printConsole("Deleted Savefile.");
+		} else {
+			printConsole("Nothing to delete.");
+		}
+	}
+	
+	private static void printConsole(String s) {
+		console.append(s+"\n");
+	}
+
+	private static void clearConsole() {
+		console.setText("");
+	}
+}
+enum SERVER_STATUS {Run,Idle}
---
+++ src/de/vatterger/entitysystem/UpdateRunnable.java
@@ -0,0 +1,154 @@
+package de.vatterger.entitysystem;
+import java.util.Date;
+import java.util.concurrent.locks.LockSupport;
+
+import de.vatterger.entitysystem.interfaces.SavableWorld;
+
+public class UpdateRunnable implements Runnable {
+
+	/**The World Instance*/
+	SavableWorld w;
+	
+	boolean run,//should the gameloop continue to iterate
+			save,//is the game state being saved currently
+			debugFine;//print advanced debug messages
+	
+	float 	deltaSeconds,//The delta-value passed to the entity-system for simulation (=n*deltaNanos)
+			updateFps;//The current frames per second
+	
+	int		wantedUpdateFps;//The wanted frames per second
+
+	long	wantedDeltaNanos,//How long an gameloop-iteration should ideally take (=n/updateFps)
+			deltaNanos,//The accurate version of the time passed to the simulation
+
+			updateNanosConsumed,//How long the entity-system took to update
+			
+			wantedSleep,//How long the System should have paused
+			actualSleep,//How long the System actually paused
+			deviation,//How much actualSleep differs from wantedSleep
+			
+			elapsedTotal,//How long the Execution took
+			temp;//Temporary Variable to measure time
+	
+	public UpdateRunnable(SavableWorld w) {
+		setIsRunning(false);
+		setDebug(false);
+		save = false;
+
+		this.w = w;
+		
+		setTargetFPS(20);
+		
+		deltaNanos = 0;
+		
+		elapsedTotal = 0;
+		deviation = 0;
+	}
+	
+	@Override
+	public void run() {
+		Date beginDate = new Date();
+
+		if(debugFine) {
+			System.out.println("Thread starting up\n");
+			System.out.println("Thread started at: " + beginDate + "\n");
+		}
+
+		try {
+			w.create();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		long tempTest = System.nanoTime();
+		while (isRunning()) {
+			deltaSeconds = (float)(deltaNanos/1000000000d);//The deltatime passed to the worlds update-function
+			updateFps = 1f/deltaSeconds;
+			temp = System.nanoTime();//Current Nanotime (before game logic execution)
+
+			if(debugFine) {
+				System.out.println("Thread updated with delta = "+deltaSeconds*1000f+"ms");
+				System.out.println("Updating took: "+updateNanosConsumed/1000000f+"ms");
+				System.out.println("Wanted Sleeptime: "+wantedSleep/1000000f+"ms");
+				System.out.println("Actual Sleeptime: "+actualSleep/1000000f+"ms");
+				System.out.println("Deviation: "+deviation/1000000f+"ms"+"\n");
+			}
+			
+			w.update(deltaSeconds);//Update the World
+			
+			updateNanosConsumed = System.nanoTime()-temp;//How long the updating took
+			
+			temp = System.nanoTime();//Current Nanotime (after game logic execution)
+			
+			wantedSleep = wantedDeltaNanos - updateNanosConsumed + deviation;//How long the Thread should ideally pause
+			
+			if(0L < wantedSleep) {//decide if update-time is in range of (0, Long.MAXVALUE)
+				LockSupport.parkNanos(wantedSleep);//Try to pause the Thread for fitting amount of time
+			}
+
+			actualSleep = System.nanoTime()-temp;//How long the pause actually lasted
+			deviation = wantedSleep-actualSleep;//This time difference needs to be made up for in the next iteration
+			deltaNanos = updateNanosConsumed + actualSleep;//the total time needed for this iteration, it is passed to the update-function in the next frame
+			
+			elapsedTotal+=deltaNanos;//Measure how long the execution has been going since startup
+		}
+		tempTest = System.nanoTime()-tempTest;
+
+		w.dispose();
+		
+		Date finishDate = new Date();
+		if(debugFine) {
+			System.out.println("Total simulation-time: " + tempTest/1000000f+"ms \n");
+			System.out.println("Thread finished at: " + finishDate + " ["+elapsedTotal/1000000f+"ms]\n");
+		}
+	}
+	
+	public void setDebug(boolean debugOn){
+		debugFine = debugOn;
+	}
+	
+	public boolean getIsDebug(){
+		return debugFine;
+	}
+	
+	public void stopSimulation() {
+		setIsRunning(false);
+	}
+
+	public void startSimulation() {
+		setIsRunning(true);
+		Thread t = 	new Thread(this);
+		t.setName("UpdateRunnable");
+		t.setPriority(Thread.MAX_PRIORITY);
+		t.start();
+	}
+	
+	private boolean isRunning(){
+		return run;
+	}
+	
+	private void setIsRunning(boolean run) {
+		this.run = run;
+	}
+	
+	public float getCurrentFPS(){
+		return updateFps;
+	}
+	
+	public void setTargetFPS(int fps){
+		wantedUpdateFps = fps;
+		wantedDeltaNanos = (long)((1f/wantedUpdateFps)*1000000000);
+	}
+	
+	public SavableWorld getWorld(){
+		return this.w;
+	}
+
+	@Override
+	public String toString() {
+		if(isRunning()) {
+			return "UpdateRunnable running World: "+w.toString();
+		} else {
+			return "UpdateRunnable idling World: "+w.toString();
+		}
+	}
+}
