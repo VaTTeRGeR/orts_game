57154b716dad003fa5808ed050a362017dd8af2a
Florian <flo.schmickmann@googlemail.com>
2015-09-11 00:16:56 +0200

    New Processor

---
+++ src/de/vatterger/entitysystem/processors/ClientInputProcessor.java
@@ -0,0 +1,70 @@
+package de.vatterger.entitysystem.processors;
+
+import java.util.HashMap;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
+import com.artemis.utils.Bag;
+import com.esotericsoftware.kryonet.Connection;
+
+import de.vatterger.entitysystem.components.ClientConnection;
+import de.vatterger.entitysystem.components.DataBucket;
+import de.vatterger.entitysystem.components.ViewFrustum;
+import de.vatterger.entitysystem.netservice.MessageIn;
+import de.vatterger.entitysystem.netservice.MessageOut;
+import de.vatterger.entitysystem.netservice.NetworkService;
+import de.vatterger.entitysystem.networkmessages.ClientViewportUpdate;
+import de.vatterger.entitysystem.networkmessages.PacketBundle;
+import de.vatterger.entitysystem.util.Constants;
+
+public class ClientInputProcessor extends EntityProcessingSystem {
+
+	private ComponentMapper<ViewFrustum> vfm;
+	private ComponentMapper<ClientConnection> ccm;
+	
+	private HashMap<Connection, ClientViewportUpdate> updates = new HashMap<Connection, ClientViewportUpdate>();
+
+	private NetworkService nws = NetworkService.instance();
+
+	@SuppressWarnings("unchecked")
+	public ClientInputProcessor() {
+		super(Aspect.getAspectForAll(ViewFrustum.class, ClientConnection.class));
+	}
+
+	@Override
+	protected void initialize() {
+		vfm = world.getMapper(ViewFrustum.class);
+		ccm = world.getMapper(ClientConnection.class);
+	}
+	
+	@Override
+	protected void inserted(Entity e) {
+		updates.put(ccm.get(e).connection, new ClientViewportUpdate(vfm.get(e).rect));
+	}
+	
+	@Override
+	protected void removed(Entity e) {
+		updates.remove(ccm.get(e).connection);
+	}
+	
+	@Override
+	protected void begin() {
+		MessageIn msg = null;
+		while ((msg = nws.getNextMessage()) != null) {
+			if(msg.getObject() instanceof ClientViewportUpdate) {
+				updates.put(msg.getConnection(), (ClientViewportUpdate)msg.getObject());
+			}
+		}
+	}
+
+	@Override
+	protected void process(Entity e) {
+		vfm.get(e).rect.set(updates.get(ccm.get(e).connection).view);
+	}
+	
+	@Override
+	protected void dispose() {
+	}
+}
